c3f2233fbb8ffaf4024cdd6fa55cca4c78875013
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-12-23 20:15:58 +0000

    Added missing class definitions.

--- Server/SpeedCamerasServer.cs
+++ Server/SpeedCamerasServer.cs
@@ -158,8 +158,19 @@ namespace CloudRP.SpeedCameras
                     }
                 }
             }
-
         }
+    }
+    public class SpeedCamera
+    {
+        public Vector3 position { get; set; }
+        public Vector3 camPropPos { get; set; }
+        public float range { get; set; }
+        public int speedLimit { get; set; }
+    }
 
+    public class SpeedFine
+    {
+        public int speed { get; set; }
+        public int finePrice { get; set; }
     }
 }
