b7e1638d29160c35538676cf2ef659166c42204b
UncleMle <62445503+UncleMle@users.noreply.github.com>
2023-12-24 13:27:49 +0000

    Fixed bug where players would get flashed up twice when entering the motorway.

--- Server/SpeedCamerasServer.cs
+++ Server/SpeedCamerasServer.cs
@@ -12,23 +12,14 @@ namespace CloudRP.SpeedCameras
         public string _speedCameraDataIdentifier = "speedCameraColshapeData";
         public List<SpeedCamera> cameras = new List<SpeedCamera>
         {
-            new SpeedCamera
-            {
-                position = new Vector3(431.9, -548.4, 28.8),
-                camPropPos = new Vector3(399.9, -561.0, 27.1),
-                camFlashPos = new Vector3(400.1, -560.8, 32.7),
-                camRot = 155,
-                range = 10,
-                speedLimit = 80,
-            },
-            new SpeedCamera
-            {
-                position = new Vector3(425.8, -536.1, 28.7),
-                camPropPos = new Vector3(399.9, -561.0, 27.1),
-                camFlashPos = new Vector3(400.1, -560.8, 32.7),
-                camRot = 155,
-                range = 10,
-                speedLimit = 80,
+             new SpeedCamera
+             {
+                 position = new Vector3(431.9, -548.4, 28.8),
+                 camPropPos = new Vector3(399.9, -561.0, 27.1),
+                 camFlashPos = new Vector3(400.1, -560.8, 32.7),
+                 camRot = 155,
+                 range = 10,
+                 speedLimit = 80,
             },
             new SpeedCamera
             {
